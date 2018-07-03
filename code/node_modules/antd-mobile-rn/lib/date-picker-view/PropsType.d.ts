033299abef78855fe46c51edb71f4aa1df788eee
Tong Ruizhe <tongruizhe@sjtu.edu.cn>
2018-07-04 00:38:56 +0800

    react-native 环境修改

--- code/node_modules/antd-mobile-rn/lib/date-picker-view/PropsType.d.ts
+++
@@ -1,8 +0,0 @@
-import { DatePickerPropsType } from '../date-picker/PropsType';
-export interface DatePickerProps extends DatePickerPropsType {
-    onScrollChange?: (newValue: any, vals: any, index: number) => void;
-    /** rn only */
-    triggerTypes?: string;
-    /** rn only */
-    styles?: any;
-}
