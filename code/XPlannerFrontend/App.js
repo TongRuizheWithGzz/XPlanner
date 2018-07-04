668ed9ae3b86325aadce8ab548052a8db0ceea77
Tong Ruizhe <tongruizhe@sjtu.edu.cn>
2018-07-04 14:10:24 +0800

    将expo的demo迁移到native上

--- code/XPlannerFrontend/App.js
+++ code/XPlannerFrontend/App.js
@@ -12,6 +12,10 @@ import {
     View
 } from 'react-native';
 import {Button} from 'antd-mobile-rn';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import AppNavigator from './navigation/AppNavigator';
+
+
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' +
@@ -21,40 +25,25 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
 export default class App extends Component<Props> {
+    p() {
+        console.log("");
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
-                <Button>This a Button from antd-mobile</Button>
+                <AppNavigator />
             </View>
         );
     }
 }
 
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+        backgroundColor: '#fff',
     },
 });
