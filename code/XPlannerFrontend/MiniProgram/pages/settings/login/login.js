757122885b70cd9725017c7ea68478bfc141c972
bigface008 <bigface00@sjtu.edu.cn>
2018-07-23 17:17:51 +0800

    sync

--- code/XPlannerFrontend/MiniProgram/pages/settings/login/login.js
+++ code/XPlannerFrontend/MiniProgram/pages/settings/login/login.js
@@ -7,6 +7,7 @@ var wrapper = require("../../../interface/wrapper/wrapper");
 var time = require("../../../common/time");
 var schedule = require("../../../common/schedule");
 var extension = require("../../../common/extension");
+var api = require("../../../interface/config/api");
 
 Page({
   data: {
@@ -39,76 +40,73 @@ Page({
   login: function () {
     console.log("login");
     /* 向后端发送用户名和密码 */
-    // wrapper.loginByUsernamePassword(this.data.name, this.data.password).then((errno) => {
-    //   wx.getStorage({
-    //     key: 'date',
-    //     success: function (res) {
-    //       app.globalData.date = res.data;
-    //       app.globalData.year = parseInt(res.data.slice(0, 4));
-    //       app.globalData.month = parseInt(res.data.slice(5, 7));
-    //       app.globalData.day = parseInt(res.data.slice(8, 10));
-    //     },
-    //     fail: function() {
-    //       var tmp_date = new Date();
-    //       app.globalData.year = tmp_date.getFullYear();
-    //       app.globalData.month = tmp_date.getMonth() + 1;
-    //       app.globalData.day = tmp_date.getDate();
-    //       app.globalData.date = time.getDateStringWithZero(app.globalData.year, app.globalData.month, app.globalData.day);
-    //       console.log(app.globalData.date);
-    //       wx.setStorage({
-    //         key: 'date',
-    //         data: app.globalData.date
-    //       });
-    //     }
-    //   })
-    //   // app.globalData.date = "2018-07-18";
-    //   // app.globalData.year = 2018;
-    //   // app.globalData.month = 7;
-    //   // app.globalData.day = 18;
-    //   app.globalData.logined = true;
-    // }).catch((errno) => {
-    //   switch (errno) {
-    //     case 1:
-    //       console.log("wrong name or password");
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // });
+    wrapper.loginByUsernamePassword(this.data.name, this.data.password).then((errno) => {
+      wx.getStorage({
+        key: 'date',
+        success: function (res) {
+          app.globalData.date = res.data;
+          app.globalData.year = parseInt(res.data.slice(0, 4));
+          app.globalData.month = parseInt(res.data.slice(5, 7));
+          app.globalData.day = parseInt(res.data.slice(8, 10));
+        },
+        fail: function () {
+          var tmp_date = new Date();
+          app.globalData.year = tmp_date.getFullYear();
+          app.globalData.month = tmp_date.getMonth() + 1;
+          app.globalData.day = tmp_date.getDate();
+          app.globalData.date = time.getDateStringWithZero(app.globalData.year, app.globalData.month, app.globalData.day);
+          console.log(app.globalData.date);
+          wx.setStorage({
+            key: 'date',
+            data: app.globalData.date
+          });
+        }
+      })
+      app.globalData.logined = true;
+    }).catch((errno) => {
+      switch (errno) {
+        case 1:
+          console.log("wrong name or password");
+          break;
+        default:
+          break;
+      }
+    });
 
-    // wx.getStorage({
-    //   key: 'date',
-    //   success: function (res) {
-    //     app.globalData.date = res.data;
-    //     app.globalData.year = parseInt(res.data.slice(0, 4));
-    //     app.globalData.month = parseInt(res.data.slice(5, 7));
-    //     app.globalData.day = parseInt(res.data.slice(8, 10));
-    //   },
-    //   fail: function () {
-    //     var tmp_date = new Date();
-    //     app.globalData.year = tmp_date.getFullYear();
-    //     app.globalData.month = tmp_date.getMonth() + 1;
-    //     app.globalData.day = tmp_date.getDate();
-    //     app.globalData.date = time.getDateStringWithZero(app.globalData.year, app.globalData.month, app.globalData.day);
-    //     console.log(app.globalData.date);
-    //     wx.setStorage({
-    //       key: 'date',
-    //       data: app.globalData.date
-    //     });
-    //   }
-    // })
-    app.globalData.logined = true;
-    app.globalData.date = "2018-07-18";
-    app.globalData.year = 2018;
-    app.globalData.month = 7;
-    app.globalData.day = 18;
-    app.globalData.userInfo = userInfo;
-    app.globalData.extensions = extension.warpExtensions(extensions);
+
+    /* 获取用户信息 */
+    wrapper.wxRequestWrapper(api.queryUserInfo, "GET", {}).then((data) => {
+      app.globalData.userInfo = data;
+    }).catch((errno) => {
+      console.log("something wrong when get user info: " + errno);
+    });
+
+    /* 获取当天日程 */
+    wrapper.wxRequestWrapper(api.queryScheduleitemByDay, "GET", {
+      date: app.globalData.date
+    }).then((errno) => {
+      app.globalData.scheduleItems = schedule.warpScheduleItems(data); // 设置对应全局变量
+    }).catch((errno) => {
+      console.log("something wrong when get scheduleItems: " + errno);
+    });
+
+    /* 获取当月有日程的日期的对象 */
+    wrapper.wxRequestWrapper(api.queryDaysHavingScheduletimesInMonth, "GET", {
+      year: app.globalData.year, month: app.globalData.month
+    }).then((data) => {
+      app.globalData.dayWithItem = data; // 设置对应全局变量
+    }).catch((errno) => {
+      console.log("something wrong when get days with items in selected month: " + errno);
+    });
+
+    /* 获取用户安装扩展对应的数组 */
+    wrapper.wxRequestWrapper(api.queryEnabledExtensionsArray, "GET", {}).then((data) => {
+      app.globalData.extensions = extension.warpExtensions(data); // 设置对应全局变量
+    }).catch((errno) => {
+      console.log("something wrong when get installed extensions: " + errno);
+    })
+
     app.globalData.userFoodEaten = [];
-    app.globalData.scheduleItems = schedule.warpScheduleItems(scheduleItems);
-    app.globalData.dayWithItem = { 17: [1], 18: [3], 20: [2] };
-    app.globalData.ifAddSchedule = false;
-    app.globalData.ifSameDay = false;
 
     wx.switchTab({
       url: '/pages/schedular/schedular',
