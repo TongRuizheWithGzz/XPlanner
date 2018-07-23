0aaf2f2d5660a03416bf72274f324d3b4401b505
bigface008 <bigface00@sjtu.edu.cn>
2018-07-23 15:13:28 +0800

    Modify spider.

--- code/XPlannerFrontend/MiniProgram/extensions/spider/spider.js
+++ code/XPlannerFrontend/MiniProgram/extensions/spider/spider.js
@@ -1,44 +1,141 @@
+//index.js 获取应用实例
+var sedata = require("../../data/tongquitems.js")
 var app = getApp();
-var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 Page({
   data: {
-    scheduleItems: app.globalData.scheduleItems,
-    height: 0,
-    fix: false,
-    hideFixTop: true,
-    time: 0,
+    animationlist: [],
+    isAnimation: false,
+    zindex: [
+      1, 2, 3
+    ],
+    animationlistyet: [],
+    spiderItems: [1, 2, 3],
+    pageNumber: 1,
+    x1: -1,
+    x2: -1,
+    page: 1,
+    crawled: sedata,
   },
-  onLoad: function () {
+  onShow: function() {
+    console.log(this.data.crawled);
+    //load page 1
+  },
+  start: function(e) {
     this.setData({
-      height: 86.796875 * (this.data.scheduleItems.length) + 200,
+      x1: e.changedTouches[0].pageX,
     })
   },
-  onPullDownRefresh:function(){
-    wx.showNavigationBarLoading();
-    wx.stopPullDownRefresh();
-  },
-  detail: function (event) {
-    wx.navigateTo({
-      url: '/pages/schedular/scheduleDetails/scheduleDetails?id=' +
-      event.currentTarget.dataset.id
-      + '&day=' +
-      this.data.tabs[this.data.activeIndex],
+  end: function(e) {
+    this.setData({
+      x2: e.changedTouches[0].pageX,
     })
-  },
-  
-  onPageScroll: function (e) {
-
-    if (e.scrollTop > 228) {
+    if (this.data.x1 === this.data.x2)
+      return;
+    if (this.data.x1 > this.data.x2) {
       this.setData({
-        hideFixTop: false,
+        pageNumber: this.data.pageNumber + 1
+      })
+    } else if (this.data.x1 < this.data.x2) {
+      if (this.data.pageNumber === 1)
+        return;
+      this.setData({
+        pageNumber: this.data.pageNumber - 1
       });
-      this.setData({
-        time: parseInt((e.scrollTop - 228) / 304.390625) * 3,
-      })
     }
-    else
-      this.setData({
-        hideFixTop: true,
-      })
+    this.slidethis()
+  },
+  //事件处理函数
+  slidethis: function(e) {
+    console.log("slide")
+    var self = this;
+    if (this.data.isAnimation) {
+      return false;
+    }
+    this.setData({
+      isAnimation: true
+    });
+    var animation1 = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'cubic-bezier(.8,.2,.1,0.8)'
+    });
+    this.animation1 = animation1;
+    this
+      .animation1
+      .translateY(8)
+      .translateX(36)
+      .rotate(0)
+      .scale(1)
+      .step();
+    var animation2 = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'cubic-bezier(.8,.2,.1,0.8)'
+    });
+    this.animation2 = animation2;
+    this
+      .animation2
+      .translateY(42)
+      .translateX(10)
+      .rotate(0)
+      .step();
+    var animation3 = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'cubic-bezier(.8,.2,.1,0.8)'
+    });
+    this.animation3 = animation3;
+    this
+      .animation3
+      .translateY(24)
+      .translateX(26)
+      .rotate(0)
+      .step();
+    if (this.data.animationlistyet.length <= 0) {
+      this.data.animationlistyet = [
+        this.animation1.export(),
+        this.animation2.export(),
+        this.animation3.export()
+      ];
+    }
+    this.setData({
+      animationlist: this.data.animationlistyet
+    });
+    var animationlistyet = self.data.animationlistyet;
+    var animation = self
+      .data
+      .animationlistyet
+      .pop();
+    self
+      .data
+      .animationlistyet
+      .unshift(animation);
+    self.setData({
+      animationlist: [],
+      animationlistyet: self.data.animationlistyet
+    });
+    setTimeout(function() {
+      var zindex = self.data.zindex;
+      var slidethis = self
+        .data
+        .zindex
+        .shift();
+      self
+        .data
+        .zindex
+        .push(slidethis);
+      self.setData({
+        isAnimation: false,
+        zindex: self.data.zindex
+      });
+    }, 100);
+  },
+  refresh: function(e) {
+    this.slidethis()
+    this.setData({
+      pageNumber: 1
+    })
+  },
+  detail:function(){
+    wx.navigateTo({
+      url: '/pages/schedular/scheduleDetails/scheduleDetails',
+    })
   }
-})
+});
--- code/XPlannerFrontend/MiniProgram/pages/settings/login/login.js
+++ code/XPlannerFrontend/MiniProgram/pages/settings/login/login.js
@@ -8,45 +8,6 @@ var time = require("../../../common/time");
 var schedule = require("../../../common/schedule");
 var extension = require("../../../common/extension");
 
-// function warpExtensions(extensions_raw) {
-//   var result = [];
-//   for (var i = 0; i < extensions_raw.length; i++) {
-//     var tmp = new Object();
-//     tmp.id = extensions_raw[i].planner_id;
-//     tmp.name = extensions_raw[i].planner_name;
-//     tmp.description = extensions_raw[i].description;
-//     tmp.content = "尚无内容。";
-//     tmp.visible = false;
-//     tmp.messages = extensions_raw[i].planner_name == "Spider" ? 8 : -1;
-//     tmp.icon = extensions_raw[i].picture_path_name;
-//     var name = tmp.name.toLowerCase();
-//     tmp.extension_path = "/extensions/" + name + "/" + name;
-//     result.push(tmp);
-//   }
-//   return result;
-// }
-
-// function warpScheduleItems(scheduleItem_raw) {
-//   var array = [];
-//   for (var i = 0; i < scheduleItem_raw.length; i++) {
-//     var tmp = new Object();
-//     tmp.title = scheduleItem_raw[i].title;
-//     tmp.start_time = scheduleItem_raw[i].start_time;
-//     tmp.end_time = scheduleItem_raw[i].end_time;
-//     tmp.description = scheduleItem_raw[i].description;
-//     tmp.address = scheduleItem_raw[i].address;
-//     tmp.scheduleItem_id = scheduleItem_raw[i].scheduleItem_id;
-//     tmp.user_id = scheduleItem_raw[i].user_id;
-//     tmp.start_concret_time = tmp.start_time.slice(11, 16);
-//     tmp.end_concret_time = tmp.end_time.slice(11, 16);
-//     tmp.start_date = tmp.start_time.slice(0, 10);
-//     tmp.completed = false;
-//     tmp.visible = true;
-//     array.push(tmp);
-//   }
-//   return array;
-// }
-
 Page({
   data: {
     name: "",
@@ -115,39 +76,39 @@ Page({
     //   }
     // });
 
-    wx.getStorage({
-      key: 'date',
-      success: function (res) {
-        app.globalData.date = res.data;
-        app.globalData.year = parseInt(res.data.slice(0, 4));
-        app.globalData.month = parseInt(res.data.slice(5, 7));
-        app.globalData.day = parseInt(res.data.slice(8, 10));
-      },
-      fail: function () {
-        var tmp_date = new Date();
-        app.globalData.year = tmp_date.getFullYear();
-        app.globalData.month = tmp_date.getMonth() + 1;
-        app.globalData.day = tmp_date.getDate();
-        app.globalData.date = time.getDateStringWithZero(app.globalData.year, app.globalData.month, app.globalData.day);
-        console.log(app.globalData.date);
-        wx.setStorage({
-          key: 'date',
-          data: app.globalData.date
-        });
-      }
-    })
+    // wx.getStorage({
+    //   key: 'date',
+    //   success: function (res) {
+    //     app.globalData.date = res.data;
+    //     app.globalData.year = parseInt(res.data.slice(0, 4));
+    //     app.globalData.month = parseInt(res.data.slice(5, 7));
+    //     app.globalData.day = parseInt(res.data.slice(8, 10));
+    //   },
+    //   fail: function () {
+    //     var tmp_date = new Date();
+    //     app.globalData.year = tmp_date.getFullYear();
+    //     app.globalData.month = tmp_date.getMonth() + 1;
+    //     app.globalData.day = tmp_date.getDate();
+    //     app.globalData.date = time.getDateStringWithZero(app.globalData.year, app.globalData.month, app.globalData.day);
+    //     console.log(app.globalData.date);
+    //     wx.setStorage({
+    //       key: 'date',
+    //       data: app.globalData.date
+    //     });
+    //   }
+    // })
     app.globalData.logined = true;
-    // app.globalData.date = "2018-07-18";
-    // app.globalData.year = 2018;
-    // app.globalData.month = 7;
-    // app.globalData.day = 18;
-    // app.globalData.userInfo = userInfo;
-    // app.globalData.extensions = extension.warpExtensions(extensions);
-    // app.globalData.userFoodEaten = [];
-    // app.globalData.scheduleItems = schedule.warpScheduleItems(scheduleItems);
-    // app.globalData.dayWithItem = { 17: [1], 18: [3], 20: [2] };
-    // app.globalData.ifAddSchedule = false;
-    // app.globalData.ifSameDay = false;
+    app.globalData.date = "2018-07-18";
+    app.globalData.year = 2018;
+    app.globalData.month = 7;
+    app.globalData.day = 18;
+    app.globalData.userInfo = userInfo;
+    app.globalData.extensions = extension.warpExtensions(extensions);
+    app.globalData.userFoodEaten = [];
+    app.globalData.scheduleItems = schedule.warpScheduleItems(scheduleItems);
+    app.globalData.dayWithItem = { 17: [1], 18: [3], 20: [2] };
+    app.globalData.ifAddSchedule = false;
+    app.globalData.ifSameDay = false;
 
     wx.switchTab({
       url: '/pages/schedular/schedular',
