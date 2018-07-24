69066c44a83dc6eb31293c921ac0833d4b07b092
Tong Ruizhe <tongruizhe@sjtu.edu.cn>
2018-07-24 15:27:21 +0800

    添加DDL和一些爬取的数据

--- code/Database/XPlannerDDL.sql
+++ code/Database/XPlannerDDL.sql
@@ -11,7 +11,7 @@ drop table if exists role;
 drop table if exists JAccount_user;
 drop table if exists Weixin_user;
 drop table if exists user;
-
+drop table if exists notification;
 
 CREATE TABLE user (
   user_id       INTEGER auto_increment,
@@ -22,6 +22,21 @@ CREATE TABLE user (
   PRIMARY KEY (user_id)
 );
 
+create table notification (
+  notification_id integer primary key,
+  title           varchar(256),
+  description     varchar(2056),
+  start_time      char(16) not null,
+  end_time        char(16) not null,
+  address         varchar(256),
+  imageUrl        varchar(1024),
+  website         varchar(64),
+  UUID            varchar(64) default '',
+  create_time     timestamp(3)
+);
+
+create index create_time_index
+  on notification (create_time);
 
 create table JAccount_user (
   user_id       INTEGER,
@@ -175,5 +190,3 @@ insert into user_role values (null, 1, 2);
 insert into user_role values (null, 1, 1);
 
 #insert schedule itmes
-
-
